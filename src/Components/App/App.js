3d45a8db899184958256b9677ee602bfa7cf194a
TomAyling <tom@aerialapplications.co>
2018-08-31 09:48:14 -0400

    update

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,12 +1,12 @@
 import React, { Component } from 'react';
-import './Components/App';
+import './App.css';
+import '../Components/App/App';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header>
         <p className="App-intro">
