88280d842cff79f38f28b70d322f296ff664c787
TomAyling <tom@aerialapplications.co>
2018-09-01 09:24:53 -0400

    weekend update1

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,17 +1,19 @@
 import React, { Component } from 'react';
 import './App.css';
-import '../Components/App/App';
+
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div>
+        <h1>Ja<span class="highlight">mmm</span>ing</h1>
+        <div className="App">
+          <SearchBar />
+          <div class="App-playlist">
+            <SearchResults />
+            <Playlist />
+          </div>
+        </div>
       </div>
     );
   }
